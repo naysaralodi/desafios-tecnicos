71a08177c2648b6b2127b95a9246f7cd6d19ef77
Naysara Lodi <naysaralodi@gmail.com>
2025-05-09 12:25:46 -0300

    Create challenge5.js

---
+++ challenge5.js
@@ -0,0 +1,10 @@
+function inverterString(str) {
+    let invertida = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        invertida += str[i];
+    }
+    return invertida;
+}
+
+const entrada = 'exemplo'; 
+console.log('String invertida:', inverterString(entrada));
